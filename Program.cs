34901c912eff62412e026323bd9626dd13a90618
YliSantana <yliannysantana@gmail.com>
2025-01-22 17:47:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// año bisiesto 
+int year=0;
+
+
+Console.Write("Introduce un año: ");
+year = Convert.ToInt32(Console.ReadLine());
+ if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+     Console.WriteLine("{0} es un año bisiesto",year);
+else
+    Console.WriteLine("{0} no es un año bisiesto",year);
+ 
+Console.ReadLine();
+   
